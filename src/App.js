a84cfdc72c698c840251a8724c5326b252703dac
Jen Valladares <lilspikey333@yahoo.com>
2020-01-06 09:56:31 -0600

    inital api call verification

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const deckUrl = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      deck: {}
+    };
+  }
+
+  fetchDeck = () => {
+    fetch(deckUrl)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ deck: res });
+      });
+  };
+
+  componentDidMount() {
+    this.fetchDeck();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.deck.deck_id}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
